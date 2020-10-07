d03ecc5046e15165a6c9810198397bb6adfa7c2d
Yusuf <kkyusufk@gmail.com>
2020-10-08 04:07:06 +0530

    lets see x2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,12 +3,15 @@ import { Layout } from './src/components/layoutC/layout';
 import { Provider } from './src/context/navContext';
 
 export function wrapPageElement({ element }) {
+  console.log(element)
   // TODO: maybe make this a little pretty
   return ( 
-    <Provider>
-      <Layout>
-        {element}
-      </Layout>
-    </Provider>
+    <Layout>
+      {element}
+    </Layout>
   )
+}
+
+export function wrapRootElement({ element }) {
+  return <Provider>{element}</Provider>
 }
